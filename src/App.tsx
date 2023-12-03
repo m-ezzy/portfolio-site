ead5355a55c7b555504fb267289db71eb218e6db
Murtaza Ezzy <murtazaezzy1001@gmail.com>
2023-12-03 19:55:35 +0530

    used github API for repos

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import './styles/animation.css'
 function App() {
   return (
     <div className="app">
+    {/* <BrowserRouter> */}
     <BrowserRouter basename='/portfolio-site/'>
       <Navbar />
       <Routes>
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,8 +1,41 @@
+import { useEffect, useState } from "react"
 
 function Projects() {
+  let [repos, setRepos] = useState([]) as any[]
+
+  useEffect(() => {
+    function fetchProjects() {
+      fetch("https://api.github.com/users/m-ezzy/repos")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data)
+          setRepos(data)
+        })
+    }
+    fetchProjects()
+  }, [])
+
+  let reposItems = repos.map((repo: any) => (
+    <div className="projects-item">
+      <a href={repo.html_url}>{repo.name}</a>
+      <div className="description">{repo.description}</div>
+      <div className="date">last updated on: {new Date(repo.updated_at).toLocaleDateString()}</div>
+    </div>
+  ))
+
   return (
-    <div>
-      <h1>Projects</h1>
+    <div className="teleport page projects">
+      <div className="color31 content">
+        <code>{"<Projects>"}</code>
+        <div className="projects-list">
+          {reposItems}
+        </div>
+        <code>{"</Projects>"}</code>
+      </div>
+    	<div className="color32"></div>
+      <div className="color33"></div>
+      <div className="color34"></div>
+      <div className="color35"></div>
     </div>
   )
 }
